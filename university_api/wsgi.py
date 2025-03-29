d1b523765ad2481e7fe380eceaf6ae7ca3acde9c
Masao <david16298@gmail.com>
2025-03-29 16:40:03 +0300

    These are APIs for students and subjects

--- university_api/wsgi.py
+++ university_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for studentApp project.
+WSGI config for university_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentApp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "university_api.settings")
 
 application = get_wsgi_application()
